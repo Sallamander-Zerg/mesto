98cffdde42130350ab19fe3d37d6841bebffe4b2
SALL <62800760+Sallamander-Zerg@users.noreply.github.com>
2022-09-09 19:57:20 +0700

    js fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,32 @@
-const popupcontaner =document.querySelectorAll('.popup');
-let closeaddform= document.getElementById('closeAdd');
-let newname= document.getElementById('field-jod');
-let newjob = document.getElementById('field-name');
+const popupContaner =document.querySelectorAll('.popup');
+const popupAddProfile=document.querySelector('.js-popup-Add');
+const popupEditProfile=document.querySelector('.js-popup-edit');
+let newname= document.querySelector('.field-jod');
+let newjob = document.querySelector('.field-name');
 let job = document.querySelector('.profile__text');
 let name = document.querySelector('.profile__title');
-let form = document.getElementById('formEdit');
-let addcard = document.getElementById('formAdd');
+const form = document.querySelector('.js-popup-forEdit');
+const cardAdd = document.querySelector('.js-popup-forAdd');
 let likeButton = document.querySelectorAll('.element__like-button');
-const modalButtonsopen = document.querySelectorAll('.js-modal-active');
-const modalButtonsclose = document.querySelectorAll('.js-modal-close');
-const popupImgcontaner = document.getElementById('popupimgcontaner');
-let newimege= document.getElementById('field-src');
-let newimegename = document.getElementById('field-alt');
+const buttonAddProfile = document.querySelector('.profile__add-button');
+const buttonEditProfile  = document.querySelector('.profile__edit-button');
+const buttonsCloseProfile = document.querySelectorAll('.popup__close-butoon');
+let newimege= document.querySelector('.field-src');
+let newimegename = document.querySelector('.field-alt');
 let camelCase = document.querySelector('#card').content;
 let cards = document.querySelector('.elements');
 let element = document.querySelectorAll('.element')
 let photo = document.querySelectorAll('.element__photo');
-let Sphoto = document.querySelector('.popup__img');
-let Salt = document.querySelector('.popup__name');
-let popupimg = document.getElementById('popupimgcontaner');
-let ClosePhoto = document.getElementById('closeimg');
-let DeleteButton = document.querySelectorAll('.element__delete');
-newname.value = name.textContent;
-newjob.value = job.textContent;
+let sPhoto = document.querySelector('.popup__img');
+let sAlt = document.querySelector('.popup__name');
+const popupImgProfile = document.querySelector('.js-popup-Img');
+const buttonCloseImgProfile = document.querySelector('.popup__close-butoon-img');
+const activePopup = (popup)=>{
+  popup.classList.add('popup_active');
+ };
+ const notActivePopup = (cPopup)=>{
+  cPopup.classList.remove('popup_active');
+ };
 const initialCards = [
   {
     text:'Подольск',
@@ -77,31 +81,26 @@ const initialCards = [
     src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ]; 
-modalButtonsopen.forEach(function(item){
-  item.addEventListener('click',function Open(event){
-    event.preventDefault();
-    var modalId = item.getAttribute('data-modal');
-    modalElem = document.querySelector('.js-modal[data-modal="' + modalId + '"]');
-    modalElem.classList.add('popup_active');
-  });
-});
-
-modalButtonsclose.forEach(function(item){
-  item.addEventListener('click',function Close(event){
-    event.preventDefault();
-    var modalId = item.getAttribute('data-modal');
-    modalElem = document.querySelector('.js-modal[data-modal="' + modalId + '"]');
-    modalElem.classList.remove('popup_active');
-  });
-});
-
+buttonEditProfile.addEventListener('click', function() { 
+  activePopup(popupEditProfile); 
+  newname.value = name.textContent;
+  newjob.value = job.textContent;
+}) 
+buttonAddProfile.addEventListener('click', function() { 
+  activePopup(popupAddProfile); 
+}) 
+buttonsCloseProfile.forEach(function(butoon){
+  butoon.addEventListener('click', function() { 
+    popupContaner.forEach(function(el){
+      notActivePopup(el)
+    })
+  })
+})
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
   name.textContent = newname.value;
   job.textContent = newjob.value;
-  popupcontaner.forEach(function closeConteiner(popup){
-    popup.classList.remove('popup_active');
-  })
+  notActivePopup(popupEditProfile)
   }
 
 function addformcard(evt){
@@ -111,10 +110,7 @@ let text = newimegename.value;
 let stats = {text,src};
 let value = create_newcards(stats);
 addCards(value);
-popupcontaner.forEach(function closeConteiner(popup){
-  popup.classList.remove('popup_active');
-})
-
+notActivePopup(popupAddProfile)
 }
 
 function create_newcards(card){
@@ -129,9 +125,9 @@ function create_newcards(card){
   newCard.remove();
 });
  newCard.querySelector('.element__photo').addEventListener('click',()=>{
-  popupImgcontaner.classList.add('popup_active');
-  Sphoto.src = newCard.querySelector('.element__photo').src;
-  Salt.textContent=newCard.querySelector('.element__photo').alt;
+  activePopup(popupImgProfile);
+  sPhoto.src = newCard.querySelector('.element__photo').src;
+  sAlt.textContent=newCard.querySelector('.element__photo').alt;
  })
   return newCard;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 }
@@ -144,10 +140,33 @@ function addCards(el){
   cards.prepend(el);
 }
 form.addEventListener('submit', formSubmitHandler);
-addcard.addEventListener('submit',addformcard);
+cardAdd.addEventListener('submit',addformcard);
 
-ClosePhoto.addEventListener('click',()=>{
-  popupimg.classList.remove('popup_active');
+buttonCloseImgProfile.addEventListener('click',()=>{
+  notActivePopup(popupImgProfile);
 });
-
-
+// modalButtonsclose.forEach(function(item){
+//   item.addEventListener('click',function Close(event){
+//     event.preventDefault();
+//     var modalId = item.getAttribute('data-modal');
+//     modalElem = document.querySelector('.js-modal[data-modal="' + modalId + '"]');
+//     modalElem.classList.remove('popup_active');
+//   });
+// });
+// butoon.addEventListener('kaydown', function(event){
+  //   if(event.code==='Escape'){
+  //   popupContaner.forEach(function(el){
+  //     notActivePopup(el);
+  //   })
+  // }
+  // })
+// modalButtonsopen.forEach(function(item){
+//   item.addEventListener('click',function Open(event){
+//     event.preventDefault();
+//     var modalId = item.getAttribute('data-modal');
+//     modalElem = document.querySelector('.js-modal[data-modal="' + modalId + '"]');
+//     modalElem.classList.add('popup_active');
+//     newName.value = name.textContent;
+//     newJob.value = job.textContent;
+//   });
+// });
