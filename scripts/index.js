fa9451900cbe5d4914e1dc8384a6f93b33329ee5
SALL <62800760+Sallamander-Zerg@users.noreply.github.com>
2022-09-18 12:59:42 +0700

    error fix

--- scripts/index.js
+++ scripts/index.js
@@ -36,54 +36,67 @@ const openPopup = (popup)=>{
 const initialCards = [
   {
     text:'Подольск',
+    alt:"Истиный Подольск",
     src:'https://radiovera.ru/wp-content/uploads/2018/10/Gyustav-Dore-Ad.jpg'
   },
   {
     text:'Карачевск',
+    alt:"Замок в горах",
     src:'images/1.jpg'
   },
   {
     text:'Гора Эльбрус',
+    alt:"Закат на фоне горы Эльбрус",
     src:'images/2.jpg'
   },
   {
     text:'Домбай',
+    alt:"лес на фоне гор",
     src:'images/3.jpg'
   },
   {
     text:'Гора Эльбрус',
+    alt:"Закат на фоне горы Эльбрус",
     src:'images/4.jpg'
   },
   {
     text:'Домбай',
+    alt:"лес на фоне гор",
     src:'images/5.jpg'
   },
   {
     text:'Карачаево-Черкесия',
+    alt:"Замок в горах",
     src:'images/6.jpg'
   },
   {
     text: 'Архыз',
+    alt:"тающий снег на вершинах гор",
     src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
     text: 'Челябинская область',
+    alt:"озеро окруженное горами и виршинами",
     src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
     text: 'Иваново',
+    alt:"страшный сон Варламова",
     src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
     text: 'Камчатка',
+    alt:"поле перед горой",
     src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
     text: 'Холмогорский район',
+    alt:"железная дорога посреди леса уходящая в даль",
     src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
     text: 'Байкал',
+    alt:"склон горы перед замершим озером",
     src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ]; 
@@ -94,6 +107,8 @@ buttonEditProfile.addEventListener('click', function() {
 }) 
 buttonAddProfile.addEventListener('click', function() { 
   openPopup(popupAddProfile); 
+  newImege.value=""
+  newImegeName.value=""
 }) 
 
 function closeKey(pop){ 
@@ -110,11 +125,9 @@ function closeKey(pop){
 }
 function closeByEscape(evt) {
   if (evt.key === 'Escape') {
-    const openedPopup = document.querySelector('.popup_opened')
+    const openedPopup = document.querySelector('.popup_active')
     closePopup(openedPopup);
   }
-  document.addEventListener('keydown', closeByEscape);
-  document.removeEventListener('keydown', closeByEscape);
 }
 buttonCloseEditProfile.addEventListener('click', function() { 
   closePopup(popupEditProfile); 
@@ -126,13 +139,13 @@ function formSubmitHandler (evt) {
   evt.preventDefault(); 
   name.textContent = newName.value;
   job.textContent = newJob.value;
-  notActivePopup(popupEditProfile)
+  closePopup(popupEditProfile)
 }
 
 function addFormCard(evt){
 evt.preventDefault();
 const src  = newImege.value;
-const text = newImegeName.value;  
+const text = newImegeName.value; 
 const stats = {text,src};
 const value = createNewCards(stats);
 addCards(value);
@@ -142,7 +155,7 @@ closePopup(popupAddProfile)
 function createNewCards(card){
   const newCard =   caseContent.querySelector('.element').cloneNode(true);
   newCard.querySelector('.element__photo').setAttribute('src', card.src);
-  newCard.querySelector('.element__photo').setAttribute('alt', card.text);
+  newCard.querySelector('.element__photo').setAttribute('alt', card.alt);
   newCard.querySelector('.element__header').textContent = card.text;
   newCard.querySelector('.element__like-button').addEventListener('click', ()=>{
     newCard.querySelector('.element__like-button').classList.toggle('element__like-button_active');
@@ -167,7 +180,8 @@ function addCards(el){
 }
 formEdit.addEventListener('submit', formSubmitHandler);
 formCardAdd.addEventListener('submit',addFormCard);
-
+document.addEventListener('keydown', closeByEscape);
+document.removeEventListener('keydown', closeByEscape);
 buttonCloseImgProfile.addEventListener('click',()=>{
   closePopup(popupImgProfile);
 });
